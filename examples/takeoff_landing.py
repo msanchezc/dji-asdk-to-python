b0ade8c1f096120cc1a2ebccecd35718e65c2ffe
Carlos Tovar <cartovarc@gmail.com>
2020-08-22 11:55:33 -0500

    update takeoff landing example

--- examples/takeoff_landing.py
+++ examples/takeoff_landing.py
@@ -1,12 +1,21 @@
-import time
 from dji_asdk_to_python.products.aircraft import Aircraft
+from dji_asdk_to_python.flight_controller.flight_controller_state import FlightControllerState
+from dji_asdk_to_python.flight_controller.flight_mode import FlightMode
 
-
-APP_IP = "192.168.0.109"
+APP_IP = "YOUR_AIRCRAFT_IP"
 
 drone = Aircraft(APP_IP)
 fc = drone.getFlightController()
 
 print(fc.startTakeoff())
-time.sleep(5)
+
+while True:
+    flight_controller_state = fc.getState()
+    if not isinstance(flight_controller_state, FlightControllerState):
+        continue
+
+    flight_mode = flight_controller_state.getFlightMode()
+    if flight_controller_state.isFlying() and flight_mode != FlightMode.AUTO_TAKEOFF:
+        break
+
 print(fc.startLanding())
